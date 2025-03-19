b58d6114f632a8d86a9b378954666f5b17acdbbf
nikitaSB01 <boss.shvabauer@bk.ru>
2025-03-19 19:23:03 +0500

    правки

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "React_router-two";
-
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: "/React_router-two/",
   plugins: [react()],
 });
